88a596312fdc435a6e62cebedb55d0b80d4b29ca
CeciFerrari16 <cecilia.ferrari2@liceoariostospallanzani-re.edu.it>
2022-02-14 21:57:18 +0100

    Funzioni Salva e Leggi

--- team_ex.c
+++ team_ex.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-//#include <stdlib.h>
 #include <string.h>
 
-#define N 8
+#define N 10
 #define MAXSTR 15
+#define RUBRICA "AGENDA.txt"
 
 typedef struct {
-	int index;
 	int id;
   	char cognome[MAXSTR];
 	char nome[MAXSTR];
@@ -14,15 +13,17 @@ typedef struct {
 } s_arch;
 
 
-s_arch archivio[N]={ // define 6 
-	{0, 1,"Rossi", "Mario", 123456789},
-	{1, 2,"Verdi", "Ugo", 987654321},
-	{2, 3,"Gadioli", "Giovanni", 135798642},
-	{3, 4,"Plutone", "Pippo", 246897531},
+s_arch archivio[N]={ // define 10 
+	{1,"Rossi", "Mario", 123456789},
+	{2,"Verdi", "Ugo", 987654321},
+	{3,"Gadioli", "Giovanni", 135798642},
+	{4,"Plutone", "Pippo", 246897531},
 };
 
 int MyIndex=4;
 //indico il numero di elementi allocati nel vettore archivio
+int MyOut(int);
+int Leggi(int);
 
 void Visualizza(int pos)
 {
@@ -37,6 +38,59 @@ while(i<pos)
 	}
 }
 
+int MyOut(int pos)
+{
+  FILE *out;
+  out = fopen(RUBRICA,"w+"); //apertura del file write
+  for(int i=0; i < pos; i++){
+    if (out)
+      {
+        fprintf(out, "\n\n     ID: %d ", archivio[i].id);
+        fprintf(out, "\nCOGNOME: %s ", archivio[i].cognome);
+        fprintf(out, "\n   NOME: %s ", archivio[i].nome);
+        fprintf(out, "\n   NUMERO TELEFONO: %d ", archivio[i].numero_tel);
+        
+        printf("Sono passato di quii");
+        return 0;
+        }
+    else
+      {
+        printf("Errore apertura file");
+        return -1;
+      }
+  }fclose(out);
+}
+
+int Leggi(int pos)
+{
+  FILE *read;
+  read = fopen(RUBRICA,"r");
+  for(int i=0; i < pos; i++){
+    if (read)
+      { 
+        printf("\n\n     ID: %d ", archivio[i].id);
+        fscanf(read, "%d", &archivio[i].id);
+        printf("\nCOGNOME:");
+        fscanf(read, "%s",archivio[i].cognome);
+        printf("   NOME:");
+        fscanf(read, "%s",archivio[i].nome);
+        printf("   NUMERO TELEFONO:");
+        fscanf(read, "%d", &archivio[i].numero_tel);
+
+        printf("\n\n     ID: %d ", archivio[i].id);
+        printf("\nCOGNOME: %s ", archivio[i].cognome);
+        printf("\n   NOME: %s ", archivio[i].nome);
+        printf("\n   NUMERO TELEFONO: %d ", archivio[i].numero_tel);
+        return 0;
+      }
+    else
+      {
+        printf("Errore apertura file");
+        return -1;
+      }
+  }fclose(read);
+}
+
 int Insert(int pos)
 {
 	char invio;
@@ -48,7 +102,6 @@ int Insert(int pos)
 	return(pos);
 	}
 	archivio[pos].id = pos + 1;
-	archivio[pos].index = pos + 1;
 	
 	printf("\nNuovo Rcd n. %d:", pos + 1);
 	printf("\nCOGNOME:");
@@ -61,39 +114,42 @@ int Insert(int pos)
 	return(pos);
 }
 
+void Scambia(int i, int j)
+{
+  s_arch tmp;
+  
+  tmp.id = archivio[i].id;
+  archivio[i].id = archivio[j].id;
+  archivio[j].id = tmp.id;
+  
+  strcpy(tmp.cognome, archivio[i].cognome);
+  strcpy(archivio[i].cognome, archivio[j].cognome);
+  strcpy(archivio[j].cognome, tmp.cognome);
+  
+  strcpy(tmp.nome, archivio[i].nome);
+  strcpy(archivio[i].nome, archivio[j].nome);
+  strcpy(archivio[j].nome, tmp.nome);
+  
+  tmp.numero_tel = archivio[i].numero_tel;
+  archivio[i].numero_tel = archivio[j].numero_tel;
+  archivio[j].numero_tel = tmp.numero_tel;
+}
+
 void SortByCognome(int pos)
 {   
-    int list[pos];
+    //int list[pos];
     int t;
-    for(int i = 0; i < pos+1; i++){
-        for(int v = i+1; v < pos-1; v++){
-			printf("%d\n", strcmp(archivio[i].cognome, archivio[v].cognome));
-    	    if(strcmp(archivio[i].cognome, archivio[v].cognome) == 1){
-    	        t = archivio[v].index;
-    	        archivio[v].index = archivio[i].index;
-    	        archivio[i].index = t;
-				printf("cambio\n");
+    for(int i = 0; i < pos; i++){
+        for(int v = i+1; v < pos; v++){
+			//printf("%d\n", strcmp(archivio[i].cognome, archivio[v].cognome));
+    	    if(strcmp(archivio[i].cognome, archivio[v].cognome) > 0){
+    	        Scambia(i, v);
     	    }
-			printf("%s\n", archivio[i].cognome);
-			printf("%s\n", archivio[v].cognome);
-			printf("i: %d\n", i);
-			printf("v: %d\n", v);
         }
 	}
-
-	for(int a=0; a < pos; a++){
-		list[a] = archivio[a].index;
-	}
-
-	for(int a=0; a < pos; a++){
-    {	
-     	printf("\n\n     ID:%d", archivio[list[a]].id);
-     	printf("\n COGNOME:%s ", archivio[list[a]].cognome);
-    	printf("\n    NOME:%s ", archivio[list[a]].nome);
-    	printf("\n    NUMERO TELEFONO:%d ", archivio[list[a]].numero_tel);
-    	}
-    }
+	Visualizza(MyIndex);
 }
+
 
 void VisualizzaByCognome(int pos)
 { 
@@ -171,11 +227,13 @@ int menu_scelta(void)
     printf("\n4 -> Ricerca da cognome");
     printf("\n5 -> Ricerca da nome");
     printf("\n6 -> Ricerca da telefono");
+    printf("\n7 -> Salva");
+    printf("\n8 -> Leggi");
     printf("\n" );
     printf("\nEffettua una scelta -> " );
     scanf("%d", &selezione );
     }
-    while (selezione < 0 || selezione > 6);
+    while (selezione < 0 || selezione > 8);
   return selezione;
 }
 
@@ -186,23 +244,29 @@ int main(void)
     while((scelta=menu_scelta())!=0){
     switch(scelta){
         case 1: 
-               Visualizza(MyIndex);
-               break;
+                Visualizza(MyIndex);
+                break;
         case 2:
-               MyIndex=Insert(MyIndex);
-               break;
+                MyIndex=Insert(MyIndex);
+                break;
         case 3:
-               SortByCognome(MyIndex);
-               break;
+                SortByCognome(MyIndex);
+                break;
         case 4:
-               VisualizzaByCognome(MyIndex);
-               break;
+                VisualizzaByCognome(MyIndex);
+                break;
         case 5:
-               VisualizzaByNome(MyIndex);
-               break;
+                VisualizzaByNome(MyIndex);
+                break;
         case 6:
-               VisualizzaByTel(MyIndex);
-               break;
+                VisualizzaByTel(MyIndex);
+                break;
+        case 7:
+                MyOut(MyIndex);
+                break;
+        case 8:
+                Leggi(MyIndex);
+                break;
         }
     }             
     return 0;
